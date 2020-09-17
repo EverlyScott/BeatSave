c8dfbb9f242c2759f4449b4391cbf69e27ca0295
ScribbleNerd <benjibear2007@gmail.com>
2020-09-17 00:20:21 -0500

    commit

--- app/js/global/api.js
+++ app/js/global/api.js
@@ -20,92 +20,56 @@ function download(hash) {
 //maps
 function mapDetail(hash, callback) {
   loadJSON(`${apiurl}/maps/by-hash/${hash}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
 
 function getUploader(id, page, callback) {
   loadJSON(`${apiurl}/maps/uploader/${id}/${page}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
 
 function byhot(page, callback) {
   loadJSON(`${apiurl}/maps/hot/${page}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
 
 function byRating(page, callback) {
   loadJSON(`${apiurl}/maps/rating/${page}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
 
 function byLatest(page, callback) {
   loadJSON(`${apiurl}/maps/latest/${page}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
 
 function byDownloads(page, callback) {
   loadJSON(`${apiurl}/maps/downloads/${page}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
 
 function byPlays(page, callback) {
   loadJSON(`${apiurl}/maps/plays/${page}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
 
 //search
 function search(query, page, callback) {
   loadJSON(`${apiurl}/search/text/${page}?q=${query}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
 
 //stats
 function stats(hash, callback) {
   loadJSON(`${apiurl}/stats/by-hash/${hash}`, (res) => {
-    if (callback != undefined) {
-      callback(JSON.parse(res))
-    } else {
-      return JSON.parse(res)
-    }
+    callback(JSON.parse(res))
   })
 }
